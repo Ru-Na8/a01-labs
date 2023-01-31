f10c286f987272a1f006b39b6aa3204851a1accb
Rula Nayf <rn222qf@student.lnu.se>
2023-02-01 00:05:22 +0100

    Update A01.Lab4/src/test/java/DiceTest.java

--- A01.Lab4/src/test/java/DiceTest.java
+++ A01.Lab4/src/test/java/DiceTest.java
@@ -2,29 +2,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 class DiceTest {
 
+        Dice dice = new Dice();
+
+        @Test
+        public void testroll(){
+            dice.roll();
+            int value= dice.getValue();
+            assertTrue(0<value && value<7);
+        }
 
         @Test
         public void testSetValue() {
-            Dice dice = new Dice();
             dice.setValue(3);
             int value = dice.getValue();
             assertEquals(3, value);
-
-            dice.setValue(0);
-            value = dice.getValue();
-            assertNotEquals(0, value);
-
-            dice.setValue(7);
-            value = dice.getValue();
-            assertNotEquals(7, value);
         }
 
         @Test
         public void testGetDices() {
-            Dice dice = new Dice();
             dice.roll();
             int value = dice.getDices();
             assertTrue(value >= 1 && value <= 6);
+        }
+        @Test
+        public void testToString() {
+            assertEquals("⚂-3", dice.toString());
         }
 
 }
